c43c1fe089a5527495134f54b070c8cb183fe6c2
M Imam Pratama <imambungo@gmail.com>
2022-10-22 22:54:33 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+	const fetchRepositories = async () => {
+		const response = await fetch('http://localhost:3000/repositories') // https://stackoverflow.com/q/70486828/9157799
+		const repositories = await response.json()
+		return repositories
+	}
+	const repositoriesPromise = fetchRepositories()
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Helloooo</p>
+
+{#await repositoriesPromise then repositories}
+	{JSON.stringify(repositories)}
+{/await}

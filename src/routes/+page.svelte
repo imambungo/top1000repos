5a3ee4bfe078260e22f5085e79c16eb4633394a8
M Imam Pratama <imambungo@gmail.com>
2022-10-23 00:54:25 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
-	const fetchRepositories = async () => {
-		const response = await fetch('http://localhost:3000/repositories') // https://stackoverflow.com/q/70486828/9157799
+	import { onMount } from 'svelte'; // https://stackoverflow.com/a/74165772/9157799
+
+	let repositoriesPromise = null
+
+	onMount(async () => { //  // https://stackoverflow.com/q/70486828/9157799
+		const response = await fetch('http://localhost:3000/repositories')
 		const repositories = await response.json()
-		return repositories
-	}
-	const repositoriesPromise = fetchRepositories()
+		repositoriesPromise = repositories
+	})
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Helloooo</p>
 
-{#await repositoriesPromise then repositories}
+{#await repositoriesPromise then repositories} <!-- https://svelte.dev/docs#template-syntax-await -->
 	{JSON.stringify(repositories)}
 {/await}

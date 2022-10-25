1991f7bbff61664d9cceec8e1ac8838cd2d887cb
M Imam Pratama <imambungo@gmail.com>
2022-10-25 23:25:39 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,11 +16,11 @@
 	let repositoriesP
 
 	const sortByStars = () => {
-		const compareStars = (a, b) => {
+		const compareStars = (a, b) => { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 			if (a.stargazers_count > b.stargazers_count)
-				return -1
+				return -1 // a first, then b
 			if (a.stargazers_count < b.stargazers_count)
-				return 1
+				return 1 // b first, then a
 			return 0
 		}
 		repositories = repositories.sort(compareStars)

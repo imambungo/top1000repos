c36fb9a7c4b992bd0afd81479b90c1abe3be3447
M Imam Pratama <imambungo@gmail.com>
2022-10-26 10:52:27 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,42 +41,44 @@
 	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<main class="max-w-3xl mx-auto">
+	<h1>Welcome to SvelteKit</h1>
 
-<div class="max-w-3xl mx-auto">
-	Sort by:
-	<button on:click={sortByStars}>
-		stars
-	</button>
-</div>
+	<div class="max-w-3xl mx-auto">
+		Sort by:
+		<button on:click={sortByStars}>
+			stars
+		</button>
+	</div>
 
-<p>{excluded_topics.toString()}</p>
+	<p>{excluded_topics.toString()}</p>
 
-{#await repositoriesP} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/docs#template-syntax-await -->
-	<p>Hang on..</p>
-{:then}
-	wow
-	{#each repositories as repository, i} <!-- https://svelte.dev/docs#template-syntax-each -->
-		<div class="flex max-w-3xl mx-auto">
-			<div class="w-10 text-right shrink-0 mr-3"> <!-- number | shrink: https://stackoverflow.com/a/45741742/9157799 -->
-				{i+1}
-			</div>
-			<div class="grow"> <!-- the rest -->
-				<div class="flex"> <!-- repo name & stars -->
-					<div class="grow"> <!-- if we grow the <a>, the white space after the text will be clickable -->
-						<a href="{repository.html_url}" class="text-blue-600">{repository.full_name}</a>
-					</div>
-					<div>{readableNumber(repository.stargazers_count)}</div>
+	{#await repositoriesP} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/docs#template-syntax-await -->
+		<p>Hang on..</p>
+	{:then}
+		wow
+		{#each repositories as repository, i} <!-- https://svelte.dev/docs#template-syntax-each -->
+			<div class="flex">
+				<div class="w-10 text-right shrink-0 mr-3"> <!-- number | shrink: https://stackoverflow.com/a/45741742/9157799 -->
+					{i+1}
 				</div>
-				<div class="">{repository.description}</div>
-				{#if repository.topics.length > 0} <!-- mt-1 only if there's a topic -->
-					<div class="flex flex-wrap gap-1 mt-1"> <!-- topics -->
-						{#each repository.topics as topic}
-							<div on:click={excludeTopic} class="cursor-pointer rounded-full bg-sky-100 px-2 pb-0.5 text-sm text-blue-500">{topic}</div>
-						{/each}
+				<div class="grow"> <!-- the rest -->
+					<div class="flex"> <!-- repo name & stars -->
+						<div class="grow"> <!-- if we grow the <a>, the white space after the text will be clickable -->
+							<a href="{repository.html_url}" class="text-blue-600">{repository.full_name}</a>
+						</div>
+						<div>{readableNumber(repository.stargazers_count)}</div>
 					</div>
-				{/if}
+					<div class="">{repository.description}</div>
+					{#if repository.topics.length > 0} <!-- mt-1 only if there's a topic -->
+						<div class="flex flex-wrap gap-1 mt-1"> <!-- topics -->
+							{#each repository.topics as topic}
+								<div on:click={excludeTopic} class="cursor-pointer rounded-full bg-sky-100 px-2 pb-0.5 text-sm text-blue-500">{topic}</div>
+							{/each}
+						</div>
+					{/if}
+				</div>
 			</div>
-		</div>
-	{/each}
-{/await}
+		{/each}
+	{/await}
+</main>

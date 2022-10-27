a84f2e47424171cd8d00271196a8e0449236ac77
M Imam Pratama <imambungo@gmail.com>
2022-10-27 18:20:06 +0700

    s

--- src/routes/LastCommitDate.svelte
+++ src/routes/LastCommitDate.svelte
@@ -1,5 +1,43 @@
 <script>
 	export let last_commit_date
+
+	const daysAgo = () => { // https://stackoverflow.com/a/47181114/9157799
+		const startDate  = new Date(last_commit_date)
+		const endDate    = new Date()
+		const diffInMs   = endDate - startDate
+		const diffInDays = diffInMs / (1000 * 60 * 60 * 24)
+		return parseInt(diffInDays)
+	}
+
+	const format = () => {
+		const total_days = daysAgo()
+		const years      = parseInt(total_days / 365)
+		const remaining  = total_days % 365
+		const months     = parseInt(remaining / 30)
+		const days       = remaining % 30
+
+		let result = ''
+		if (years > 0) {
+			result += `${years} year`
+			if (years > 1) result += 's'
+			if (months == 0) return result + ' ago'
+		}
+		if (months > 0) {
+			if (years >= 1) result += ', '
+			result += `${months} month`
+			if (months > 1) result += 's'
+			if (years >= 1) return result + ' ago'
+		}
+		if (days > 0) {
+			if (months == 0 && days == 1) return 'yesterday'
+			if (months >= 1 && months < 3) result += ', '
+			if (months <  3) {
+				result += `${days} days`
+			}
+			return result + ' ago'
+		}
+		return 'today'
+	}
 </script>
 
-{last_commit_date}
+<p title={last_commit_date}>{format()}</p> <!-- title is tooltip -->

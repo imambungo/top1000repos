94d4a90bc326914cb45790ec13dae7f5fe3459e2
M Imam Pratama <imambungo@gmail.com>
2022-10-29 00:06:09 +0700

    s

--- src/routes/Top5PRThumbsUp.svelte
+++ src/routes/Top5PRThumbsUp.svelte
@@ -2,7 +2,7 @@
 	export let top_5_pr_thumbs_up
 
 	const color = () => {
-		if (top_5_pr_thumbs_up == 0)
+		if (top_5_pr_thumbs_up == 0 || top_5_pr_thumbs_up == null)
 			return 'text-red-600'
 		if (top_5_pr_thumbs_up <= 10)
 			return 'text-orange-400'
@@ -15,7 +15,7 @@
 
 <div class='whitespace-nowrap'>
 	Top 5 PRs thumbs up:
-	<span class='{color()}'> <!-- use span because it's an inline elemtn, so we don't need to wrap things in a flex div -->
-		{top_5_pr_thumbs_up}
+	<span class='{color()} {top_5_pr_thumbs_up == null && "font-black"}'> <!-- use span because it's an inline elemtn, so we don't need to wrap things in a flex div -->
+		{top_5_pr_thumbs_up ? top_5_pr_thumbs_up : 0} <!-- if null (no closed PR in the last 1 year), set to 0 -->
 	</span>
 </div>

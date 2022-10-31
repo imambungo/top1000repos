d4b08f2ef9f64fd1f2d11c9b03edd8c72ee3ef4b
M Imam Pratama <imambungo@gmail.com>
2022-11-01 01:47:47 +0700

    s

--- src/routes/Description.svelte
+++ src/routes/Description.svelte
@@ -2,6 +2,6 @@
 	export let description
 </script>
 
-<div class="text-sm text-gray-800">
+<div class="text-sm text-gray-800 line-clamp-4"> <!-- https://stackoverflow.com/a/41137262/9157799 | https://tailwindcss.com/docs/plugins#line-clamp -->
 	{description ? description : ''} <!-- if null, empty string -->
 </div>

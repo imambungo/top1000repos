009b4678e5863ea25f5fa0233b312143b18a3272
M Imam Pratama <imambungo@gmail.com>
2022-11-02 20:37:43 +0700

    s

--- src/routes/NumOfClosedPR.svelte
+++ src/routes/NumOfClosedPR.svelte
@@ -3,13 +3,13 @@
 	export let html_url
 
 	const color = () => {
-		if (num_of_closed_pr_since_1_year == 0 || num_of_closed_pr_since_1_year == null)
+		if (num_of_closed_pr_since_1_year <= 10 || num_of_closed_pr_since_1_year == null)
 			return 'text-red-600'
-		if (num_of_closed_pr_since_1_year <= 10)
+		if (num_of_closed_pr_since_1_year <= 50)
 			return 'text-orange-400'
-		if (num_of_closed_pr_since_1_year <= 20)
+		if (num_of_closed_pr_since_1_year <= 150)
 			return 'text-yellow-500'
-		if (num_of_closed_pr_since_1_year >= 100)
+		if (num_of_closed_pr_since_1_year >= 1000)
 			return 'text-green-500'
 	}
 
@@ -20,11 +20,16 @@
 	}
 
 	const top_5_pr_url = `${html_url}/pulls?q=is%3Apr+sort%3Areactions-%2B1-desc+is%3Aclosed+closed%3A%3E${aYearAgo()}`
+
+	const boldIfZero = () => {
+		if (num_of_closed_pr_since_1_year == 0)
+			return 'font-black'
+	}
 </script>
 
 <div class='whitespace-nowrap'>
 	<span title='*since the last 12 months'>Number of closed PRs*</span>:
-	<a href={top_5_pr_url} target='_blank' class='{color()}'>
+	<a href={top_5_pr_url} target='_blank' class='{color()} {boldIfZero()}'>
 		{num_of_closed_pr_since_1_year}
 	</a>
 </div>

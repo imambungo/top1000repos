1ad94d1a7074554694e148269374c873765c0377
M Imam Pratama <imambungo@gmail.com>
2023-01-24 20:46:39 +0700

    s

--- src/routes/Top5OpenIssueThumbsUp.svelte
+++ src/routes/Top5OpenIssueThumbsUp.svelte
@@ -16,9 +16,11 @@
 	const top_5_open_issue_url = `${html_url}/issues?q=is%3Aissue+is%3Aopen+sort%3Areactions-%2B1-desc`
 </script>
 
-<div class='whitespace-nowrap'>
-	<span title='total 👍 of the top 5 open issues (of all time)'>Top 5 open issues thumbs up</span>:
-	<a href={top_5_open_issue_url} target='_blank' class='{color()}'>
-		{total_thumbs_up_of_top_5_open_issue_of_all_time}
-	</a>
-</div>
+{#if total_thumbs_up_of_top_5_open_issue_of_all_time != 0}
+	<div class='whitespace-nowrap'>
+		<span title='total 👍 of the top 5 open issues (of all time)'>Top 5 open issues thumbs up</span>:
+		<a href={top_5_open_issue_url} target='_blank' class='{color()}'>
+			{total_thumbs_up_of_top_5_open_issue_of_all_time}
+		</a>
+	</div>
+{/if}

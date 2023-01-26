478c797155da04ef421e2abc4c4d2ac7b51f2311
M Imam Pratama <imambungo@gmail.com>
2023-01-26 22:22:32 +0700

    s

--- src/routes/Top5ClosedPRThumbsUp.svelte
+++ src/routes/Top5ClosedPRThumbsUp.svelte
@@ -7,11 +7,9 @@
 	const top_5_closed_pr_url = `${html_url}/pulls?q=is%3Apr+sort%3Areactions-%2B1-desc+is%3Aclosed+closed%3A%3E${a_year_ago()}`
 </script>
 
-{#if total_thumbs_up_of_top_5_closed_pr_since_1_year != 0}
-	<div class='whitespace-nowrap'>
-		<span title='total 👍 of the top 5 closed (possibly merged) PRs of the last 12 months'>Top 5 closed PRs (1 year) thumbs up</span>:
-		<a href={top_5_closed_pr_url} target='_blank'>
-			{total_thumbs_up_of_top_5_closed_pr_since_1_year}
-		</a>
-	</div>
-{/if}
+<div class='whitespace-nowrap'>
+	<span title='total 👍 of the top 5 closed (possibly merged) PRs of the last 12 months'>Top 5 closed PRs (1 year) thumbs up</span>:
+	<a href={top_5_closed_pr_url} target='_blank'>
+		{total_thumbs_up_of_top_5_closed_pr_since_1_year}
+	</a>
+</div>
--- src/routes/Top5OpenIssueThumbsUp.svelte
+++ src/routes/Top5OpenIssueThumbsUp.svelte
@@ -1,11 +1,12 @@
 <script>
 	export let total_thumbs_up_of_top_5_open_issue_of_all_time
 	export let html_url
+	export let has_issues_tab
 
 	const top_5_open_issue_url = `${html_url}/issues?q=is%3Aissue+is%3Aopen+sort%3Areactions-%2B1-desc`
 </script>
 
-{#if total_thumbs_up_of_top_5_open_issue_of_all_time != 0}
+{#if has_issues_tab}
 	<div class='whitespace-nowrap'>
 		<span title='total 👍 of the top 5 open issues (of all time)'>Top 5 open issues thumbs up</span>:
 		<a href={top_5_open_issue_url} target='_blank'>

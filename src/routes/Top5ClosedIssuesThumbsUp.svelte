871ed5c0e2e8a36a9da400a848f7218f333660da
M Imam Pratama <imambungo@gmail.com>
2023-01-26 21:58:18 +0700

    s

---
+++ src/routes/Top5ClosedIssuesThumbsUp.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let total_thumbs_up_of_top_5_closed_issues_since_1_year
+	export let html_url
+
+	import { a_year_ago } from '$lib/date.js' // https://kit.svelte.dev/docs/project-structure#project-files
+
+	const top_5_closed_issues_url = `${html_url}/pulls?q=is%3Apr+sort%3Areactions-%2B1-desc+is%3Aclosed+closed%3A%3E${a_year_ago()}`
+</script>
+
+{#if total_thumbs_up_of_top_5_closed_issues_since_1_year != 0}
+	<div class='whitespace-nowrap'>
+		<span title='total 👍 of the top 5 closed issues of the last 12 months'>Top 5 closed issues (1 year) thumbs up</span>:
+		<a href={top_5_closed_issues_url} target='_blank'>
+			{total_thumbs_up_of_top_5_closed_issues_since_1_year}
+		</a>
+	</div>
+{/if}

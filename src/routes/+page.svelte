a54147185613fb839c423eb886f2db886a23a108
M Imam Pratama <imambungo@gmail.com>
2023-01-28 15:41:01 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,107 +107,109 @@
 	$: excluded_repos_count = get_excluded_repos_count(repos, excluded_topics)
 </script>
 
-<div class="max-w-3xl mx-auto">
+<div class="max-w-3xl mx-auto lg:max-w-5xl">
 	<h1>GitHub Top 1000 Repositories</h1>
-	<div> <!-- options -->
-		<div>
-			Tab:
-			<button on:click={() => current_tab = 'explore'}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
-				explore ({explore_tab_repos_count})
-			</button>
-			<button on:click={() => current_tab = 'whitelist'}>
-				whitelist ({whitelist_tab_repos_count})
-			</button>
-			<button on:click={() => current_tab = 'blacklist'}>
-				blacklist ({blacklist_tab_repos_count})
-			</button>
+	<div class="lg:flex">
+		<div class="text-sm lg:w-1/4"> <!-- options -->
+			<div>
+				Tab:
+				<button on:click={() => current_tab = 'explore'}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
+					explore ({explore_tab_repos_count})
+				</button>
+				<button on:click={() => current_tab = 'whitelist'}>
+					whitelist ({whitelist_tab_repos_count})
+				</button>
+				<button on:click={() => current_tab = 'blacklist'}>
+					blacklist ({blacklist_tab_repos_count})
+				</button>
+			</div>
+
+			<div> <!-- TODO: after clicked, the button should be unclickable -->
+				Sort by:
+				<button on:click={() => sort_option = 'stargazers_count'}>
+					stars
+				</button>
+				<button on:click={() => sort_option = 'total_thumbs_up_of_top_5_closed_pr_since_1_year'}>
+					Top 5 closed PR thumbs up
+				</button>
+				<button on:click={() => sort_option = 'total_thumbs_up_of_top_5_closed_issues_since_1_year'}>
+					Top 5 closed issues thumbs up
+				</button>
+			</div>
+
+			<div>
+				Numbering:
+				<button on:click={() => numbering = 'rank'}>
+					rank
+				</button>
+				<button on:click={() => numbering = 'order'}>
+					order
+				</button>
+			</div>
+
+			<p>Excluded topics:</p>
+			<div class="flex flex-wrap gap-1 mt-1"> <!-- excluded topics -->
+				{#each excluded_topics as topic}
+					<div on:click={excludeTopicToggle} class="cursor-pointer rounded-full bg-sky-100 px-2 pb-0.5 text-sm text-blue-500">{topic}</div>
+				{/each}
+			</div>
+
+			Excluded: {excluded_repos_count}
 		</div>
 
-		<div> <!-- TODO: after clicked, the button should be unclickable -->
-			Sort by:
-			<button on:click={() => sort_option = 'stargazers_count'}>
-				stars
-			</button>
-			<button on:click={() => sort_option = 'total_thumbs_up_of_top_5_closed_pr_since_1_year'}>
-				Top 5 closed PR thumbs up
-			</button>
-			<button on:click={() => sort_option = 'total_thumbs_up_of_top_5_closed_issues_since_1_year'}>
-				Top 5 closed issues thumbs up
-			</button>
-		</div>
-
-		<div>
-			Numbering:
-			<button on:click={() => numbering = 'rank'}>
-				rank
-			</button>
-			<button on:click={() => numbering = 'order'}>
-				order
-			</button>
-		</div>
-
-		<p>Excluded topics:</p>
-		<div class="flex flex-wrap gap-1 mt-1"> <!-- excluded topics -->
-			{#each excluded_topics as topic}
-				<div on:click={excludeTopicToggle} class="cursor-pointer rounded-full bg-sky-100 px-2 pb-0.5 text-sm text-blue-500">{topic}</div>
-			{/each}
-		</div>
-
-		Excluded: {excluded_repos_count}
-	</div>
-
-	{#if all_repos.length == 0} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/docs#template-syntax-await -->
-		<p>Hang on..</p>
-	{:else}
-		<main class='flex flex-col gap-5'>
-			{#each repos as repo, index (repo.id)} <!-- the key (repo.id) is to fix the performance | https://svelte.dev/docs#template-syntax-each -->
-				<div class="flex {repo.topics.some(topic => excluded_topics.includes(topic)) && 'opacity-50'}"> <!-- dim if topics is in excluded_topics | https://stackoverflow.com/q/16312528/9157799 -->
-					<Number numbering={numbering} rank={repo.rank} order={index+1}/> <!-- NUMBER -->
-					<div class='grow flex flex-col gap-1'> <!-- the rest | grow against number -->
-						<div class='flex flex-wrap gap-2'>
-							<a href="{repo.html_url}" class="text-blue-600">{repo.full_name}</a> <!-- REPO FULL NAME -->
-							{#if repo.archived}
-								<div> <!-- this div prevent "Public archive" from expanding to the full_name height -->
-									<span class='rounded-full border-solid px-2 py-1 text-xs text-yellow-600 border border-yellow-600'>Public archive</span>
+		{#if all_repos.length == 0} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/docs#template-syntax-await -->
+			<p>Hang on..</p>
+		{:else}
+			<main class='flex flex-col gap-5 lg:w-3/4'>
+				{#each repos as repo, index (repo.id)} <!-- the key (repo.id) is to fix the performance | https://svelte.dev/docs#template-syntax-each -->
+					<div class="flex {repo.topics.some(topic => excluded_topics.includes(topic)) && 'opacity-50'}"> <!-- dim if topics is in excluded_topics | https://stackoverflow.com/q/16312528/9157799 -->
+						<Number numbering={numbering} rank={repo.rank} order={index+1}/> <!-- NUMBER -->
+						<div class='grow flex flex-col gap-1'> <!-- the rest | grow against number -->
+							<div class='flex flex-wrap gap-2'>
+								<a href="{repo.html_url}" class="text-blue-600">{repo.full_name}</a> <!-- REPO FULL NAME -->
+								{#if repo.archived}
+									<div> <!-- this div prevent "Public archive" from expanding to the full_name height -->
+										<span class='rounded-full border-solid px-2 py-1 text-xs text-yellow-600 border border-yellow-600'>Public archive</span>
+									</div>
+								{/if}
+								<div class='grow flex justify-end'>
+									{#if current_tab == 'explore'}
+										<button on:click={() => whitelistRepo(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
+											whitelist
+										</button>
+										<button on:click={() => blacklistRepo(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
+											blacklist
+										</button>
+									{:else if current_tab == 'blacklist'}
+										<button on:click={() => removeFromBlackList(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
+											remove blacklist
+										</button>
+									{:else if current_tab == 'whitelist'}
+										<button on:click={() => removeFromWhiteList(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
+											remove whitelist
+										</button>
+									{/if}
+								</div>
+							</div>
+							<Description description={repo.description}/> <!-- REPO DESCRIPTION -->
+							{#if repo.topics.length > 0} <!-- topics | if clause to prevent parent's flex gap -->
+								<div class="flex flex-wrap gap-1">
+									{#each repo.topics as topic}
+										<div on:click={excludeTopicToggle} class="cursor-pointer rounded-full bg-sky-100 px-2 py-1 text-xs text-blue-500 {excluded_topics.includes(topic) && 'line-through'}">{topic}</div>
+									{/each}
 								</div>
 							{/if}
-							<div class='grow flex justify-end'>
-								{#if current_tab == 'explore'}
-									<button on:click={() => whitelistRepo(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
-										whitelist
-									</button>
-									<button on:click={() => blacklistRepo(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
-										blacklist
-									</button>
-								{:else if current_tab == 'blacklist'}
-									<button on:click={() => removeFromBlackList(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
-										remove blacklist
-									</button>
-								{:else if current_tab == 'whitelist'}
-									<button on:click={() => removeFromWhiteList(repo.id)}> <!-- https://stackoverflow.com/q/58262380/9157799 -->
-										remove whitelist
-									</button>
-								{/if}
+							<div class='flex flex-wrap gap-x-4 text-xs text-gray-600'> <!-- last_commit_date & PRs thumbs up -->
+								<StargazersCount stargazers_count={repo.stargazers_count}/>
+								<Top5ClosedPRThumbsUp total_thumbs_up_of_top_5_closed_pr_since_1_year={repo.total_thumbs_up_of_top_5_closed_pr_since_1_year} html_url={repo.html_url}/>
+								<Top5ClosedIssuesThumbsUp total_thumbs_up_of_top_5_closed_issues_since_1_year={repo.total_thumbs_up_of_top_5_closed_issues_since_1_year} html_url={repo.html_url} has_issues_tab={repo.has_issues}/>
+								<!-- <Top5OpenIssueThumbsUp total_thumbs_up_of_top_5_open_issue_of_all_time={repo.total_thumbs_up_of_top_5_open_issue_of_all_time} html_url={repo.html_url} has_issues_tab={repo.has_issues}/> -->
+								<LastCommitDate last_commit_date={repo.last_commit_date}/>
 							</div>
-						</div>
-						<Description description={repo.description}/> <!-- REPO DESCRIPTION -->
-						{#if repo.topics.length > 0} <!-- topics | if clause to prevent parent's flex gap -->
-							<div class="flex flex-wrap gap-1">
-								{#each repo.topics as topic}
-									<div on:click={excludeTopicToggle} class="cursor-pointer rounded-full bg-sky-100 px-2 py-1 text-xs text-blue-500 {excluded_topics.includes(topic) && 'line-through'}">{topic}</div>
-								{/each}
-							</div>
-						{/if}
-						<div class='flex flex-wrap gap-x-4 text-xs text-gray-600'> <!-- last_commit_date & PRs thumbs up -->
-							<StargazersCount stargazers_count={repo.stargazers_count}/>
-							<Top5ClosedPRThumbsUp total_thumbs_up_of_top_5_closed_pr_since_1_year={repo.total_thumbs_up_of_top_5_closed_pr_since_1_year} html_url={repo.html_url}/>
-							<Top5ClosedIssuesThumbsUp total_thumbs_up_of_top_5_closed_issues_since_1_year={repo.total_thumbs_up_of_top_5_closed_issues_since_1_year} html_url={repo.html_url} has_issues_tab={repo.has_issues}/>
-							<!-- <Top5OpenIssueThumbsUp total_thumbs_up_of_top_5_open_issue_of_all_time={repo.total_thumbs_up_of_top_5_open_issue_of_all_time} html_url={repo.html_url} has_issues_tab={repo.has_issues}/> -->
-							<LastCommitDate last_commit_date={repo.last_commit_date}/>
 						</div>
 					</div>
-				</div>
-			{/each}
-		</main>
-	{/if}
+				{/each}
+			</main>
+		{/if}
+	</div>
 </div>

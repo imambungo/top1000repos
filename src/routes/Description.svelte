62cb4bd38791b7c52f3b151d031bc5512854695c
M Imam Pratama <imambungo@gmail.com>
2023-01-31 14:37:59 +0700

    s

--- src/routes/Description.svelte
+++ src/routes/Description.svelte
@@ -4,7 +4,7 @@
 
 	const replace_emoji_codes_with_actual_emojis = (description, emoji_image_urls) => {
 		Object.keys(emoji_image_urls).forEach(literal => {
-			description = description.replaceAll(`:${literal}:`, `${emoji_image_urls[literal]}`)
+			description = description.replaceAll(`:${literal}:`, `<img class="h-4 inline-block align-text-bottom" alt="${literal}" src="${emoji_image_urls[literal]}">`)
 		})
 		return description
 	}
@@ -12,6 +12,6 @@
 
 {#await promise then emoji_image_urls}
 	<div class="text-sm text-gray-800 line-clamp-4"> <!-- https://stackoverflow.com/a/41137262/9157799 | https://tailwindcss.com/docs/plugins#line-clamp -->
-		{description ? replace_emoji_codes_with_actual_emojis(description, emoji_image_urls) : ''} <!-- if null, empty string -->
+		{@html description ? replace_emoji_codes_with_actual_emojis(description, emoji_image_urls) : ''} <!-- https://stackoverflow.com/q/57580013/9157799 | if null, empty string -->
 	</div>
 {/await}

11348404ff425e4b105868d1a718e984059dbdf6
M Imam Pratama <imambungo@gmail.com>
2023-02-03 15:15:15 +0700

    s

--- src/routes/Number.svelte
+++ src/routes/Number.svelte
@@ -4,7 +4,7 @@
    export let order
 </script>
 
-<div class="w-10  shrink-0 mr-3 text-gray-700 flex justify-end"> <!-- number | shrink: https://stackoverflow.com/a/45741742/9157799 -->
+<div class="text-sm pt-0.5 w-8  shrink-0 mr-3 text-gray-700 flex justify-end"> <!-- number | shrink: https://stackoverflow.com/a/45741742/9157799 -->
    {#if numbering == 'rank'}
       <p title='rank based on the number of stars'>
          {rank}

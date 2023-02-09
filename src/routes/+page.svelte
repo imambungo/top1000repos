6620e160845a39aabaa8d2e8452ddabd57ee964a
M Imam Pratama <imambungo@gmail.com>
2023-02-09 16:25:33 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -147,7 +147,7 @@
 		{/if}
 	</div>
 	<h1 class='hidden md:block'>GitHub Top 1000 Repositories</h1> <!-- header for md screen size -->
-	<main class="px-3 sm:px-4 md:px-5 md:flex md:gap-2 lg:max-w-5xl lg:mx-auto">
+	<main class="px-3 sm:px-4 lg:px-5 md:flex md:gap-2 lg:max-w-5xl lg:mx-auto">
 		<div class="text-sm w-1/4 sticky top-8 self-start hidden md:block"> <!-- OPTIONS (for md screen size) | self-start: https://stackoverflow.com/a/66416972/9157799 -->
 			<!-- <div>
 				Sort by:

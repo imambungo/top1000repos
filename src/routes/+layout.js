ddf74d382715cdc26dd9b06afcf2bdfead1a4184
M Imam Pratama <imambungo@gmail.com>
2023-02-19 15:07:44 +0700

    add adapter-static code

---
+++ src/routes/+layout.js
@@ -0,0 +1,1 @@
+export const prerender = true; // https://kit.svelte.dev/docs/adapter-static#usage
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
 import preprocess from "svelte-preprocess"; // tailwindcss
 import adapter from "@sveltejs/adapter-auto"; // https://kit.svelte.dev/docs/adapter-auto
+import adapter from '@sveltejs/adapter-static'; // https://kit.svelte.dev/docs/adapter-static
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // these are the options: https://kit.svelte.dev/docs/adapter-static#options
+      pages: 'build', // default
+      assets: 'build', // default
+      fallback: null, // default
+      precompress: false, // default. consider to true when needed
+      strict: true // default
+    }),
   },
 
   preprocess: [

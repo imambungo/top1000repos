f1d719a3936b00ee4adf162c5b973c1d09886460
M Imam Pratama <imambungo@gmail.com>
2023-02-19 14:56:13 +0700

    comments

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess"; // tailwindcss
+import adapter from "@sveltejs/adapter-auto"; // https://kit.svelte.dev/docs/adapter-auto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

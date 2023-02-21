d660fddd223e105e01c997268bc92f53dfb32683
M Imam Pratama <imambungo@gmail.com>
2023-02-22 00:32:44 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -119,18 +119,16 @@
    }
    $: excluded_repos_count = get_excluded_repos_count(repos, excluded_topics)
 
-   let innerHeight            // https://svelte.dev/tutorial/svelte-window-bindings | svelte can't detect window "undefined" if we try to use window.innerHeight directly
-   let y_pixels_scrolled = 0  // window.scrollY
+   let scrollY = 0  // window.scrollY | https://svelte.dev/tutorial/svelte-window-bindings | svelte can't detect window "undefined" if we try to use window.scrollY directly
    let md_screen_sticky_options // https://svelte.dev/tutorial/bind-this
-   let md_screen_sticky_options_max_height = innerHeight
+   let md_screen_sticky_options_distance_to_top = 0
    $: {
-      let trigger = y_pixels_scrolled
-      let distanceToTop = md_screen_sticky_options?.getBoundingClientRect().top || 0  // https://stackoverflow.com/a/35571753/9157799
-      md_screen_sticky_options_max_height = innerHeight - distanceToTop
+      let trigger = scrollY
+      md_screen_sticky_options_distance_to_top = md_screen_sticky_options?.getBoundingClientRect().top || 0  // https://stackoverflow.com/a/35571753/9157799
    }
 </script>
 
-<svelte:window bind:scrollY={y_pixels_scrolled} bind:innerHeight/> <!-- https://svelte.dev/tutorial/svelte-window-bindings | svelte can't detect window "undefined" if we try to use window.innerHeight directly -->
+<svelte:window bind:scrollY/> <!-- https://svelte.dev/tutorial/svelte-window-bindings -->
 
 <svelte:head>
    <title>Top 1000 Repos - An unofficial web app to help you explore GitHub's top 1000 repos</title>
@@ -179,7 +177,7 @@
    </div>
    <main class="px-3 sm:px-4 lg:px-5 md:flex md:gap-2 lg:max-w-5xl lg:mx-auto">
       <div class='w-1/4 hidden md:block'> <!-- OPTIONS (for desktop / md screen size) -->
-         <div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: {md_screen_sticky_options_max_height}px;" bind:this={md_screen_sticky_options}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | Don't use CSS calc(), it doesn't work in Svelte | https://svelte.dev/tutorial/bind-this -->
+         <div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: calc(100vh - {md_screen_sticky_options_distance_to_top}px)" bind:this={md_screen_sticky_options}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | be careful with calc(): https://stackoverflow.com/q/34419813/9157799 | https://svelte.dev/tutorial/bind-this -->
             <NumberingOption numbering={numbering} handleChange={newNumbering => numbering = newNumbering}/>
             <ExcludedTopicsOption excluded_topics={excluded_topics} excluded_repos_count={excluded_repos_count} excludeTopicToggle={excludeTopicToggle}/>
          </div>

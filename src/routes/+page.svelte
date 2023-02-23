52aa7d508cfc70cf18de8edcb970f6d000e48260
M Imam Pratama <imambungo@gmail.com>
2023-02-23 19:05:46 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -229,12 +229,12 @@
       </div>
    </main>
    {#if all_repos.length != 0} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/tutorial/onmount -->
-      <footer class='text-center px-4 py-6 text-xs text-gray-700'>
+      <footer class='px-6 py-6 text-xs text-slate-500 flex flex-wrap justify-center gap-x-1'>
          <p>
             App <a class='underline' target="_blank" href="https://icons8.com/icon/103/star">icon</a> by <a class='underline' target="_blank" href="https://icons8.com">Icons8</a>.
          </p>
          <p class='md:hidden'>
-            Options <a class='underline' target="_blank" href="https://icon-icons.com/icon/options/111009">burger</a> and <a class='underline' target='_blank' href='https://icon-icons.com/icon/close/111152'>close</a> button by <a class='underline' target='_blank' href='https://icon-icons.com/users/2j7Ni9Qgct0HtmWmLGnLj/icon-sets/'>akveo</a>.
+            Option <a class='underline' target="_blank" href="https://icon-icons.com/icon/options/111009">burger</a> and <a class='underline' target='_blank' href='https://icon-icons.com/icon/close/111152'>close</a> button by <a class='underline' target='_blank' href='https://icon-icons.com/users/2j7Ni9Qgct0HtmWmLGnLj/icon-sets/'>akveo</a>.
          </p>
       </footer>
    {/if}

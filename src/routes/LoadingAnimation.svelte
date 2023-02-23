b70afdf55bdec9d2dd0447c6332d67fbb2e1e7cf
M Imam Pratama <imambungo@gmail.com>
2023-02-23 15:46:33 +0700

    s

--- src/routes/LoadingAnimation.svelte
+++ src/routes/LoadingAnimation.svelte
@@ -1,17 +1,17 @@
 <script>
    import { onMount } from 'svelte'; // https://svelte.dev/tutorial/bind-this
 
-   let dots
+   let loading
 
    onMount(() => {  // https://svelte.dev/tutorial/bind-this
       let num_of_dots = 1
       let change_num_of_dots = () => { // https://www.geeksforgeeks.org/how-to-create-frame-by-frame-animation-using-css-and-javascript/
-         dots.innerText = '.'.repeat(num_of_dots)
+         loading.innerText = 'Hang on' + '.'.repeat(num_of_dots)
          num_of_dots++;
          if (num_of_dots > 3) num_of_dots = 0
       }
       change_num_of_dots() // https://stackoverflow.com/q/10563344/9157799
-      const intervalID = setInterval(change_num_of_dots, 300) // https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+      const intervalID = setInterval(change_num_of_dots, 350) // https://developer.mozilla.org/en-US/docs/Web/API/setInterval
 
       return () => {
 			clearInterval(intervalID);
@@ -19,4 +19,4 @@
    })
 </script>
 
-<p>Hang on<span bind:this={dots}></span></p>
+<p bind:this={loading}></p>

9aacfe37423f22687eff99dcc39bec9811777b4f
M Imam Pratama <imambungo@gmail.com>
2023-02-28 01:34:03 +0700

    remove adapter-static code

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
 import preprocess from "svelte-preprocess"; // tailwindcss
-//import adapter from "@sveltejs/adapter-auto"; // https://kit.svelte.dev/docs/adapter-auto
-import adapter from '@sveltejs/adapter-static'; // https://kit.svelte.dev/docs/adapter-static
+import adapter from "@sveltejs/adapter-auto"; // https://kit.svelte.dev/docs/adapter-auto
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // these are the options: https://kit.svelte.dev/docs/adapter-static#options
-      pages: 'build', // default
-      assets: 'build', // default
-      fallback: null, // default
-      precompress: false, // default. consider to true when needed
-      strict: true // default
-    }),
+    adapter: adapter(),
   },
 
   preprocess: [

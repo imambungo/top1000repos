11a09f3b6d191b592ad9f8a1925a3511ce971f8c
M Imam Pratama <imambungo@gmail.com>
2023-03-01 18:58:15 +0700

    s

--- src/routes/MedScreenStickyOptions.svelte
+++ src/routes/MedScreenStickyOptions.svelte
@@ -1,15 +1,15 @@
 <script>
    let scrollY = 0  // window.scrollY | https://svelte.dev/tutorial/svelte-window-bindings | svelte can't detect window "undefined" if we try to use window.scrollY directly
    let element // https://svelte.dev/tutorial/bind-this
-   let md_screen_sticky_options_distance_to_top = 0
+   let elemment_distance_to_viewport_top = 0
    $: {
       let trigger = scrollY
-      md_screen_sticky_options_distance_to_top = element?.getBoundingClientRect().top || 0  // https://stackoverflow.com/a/35571753/9157799
+      elemment_distance_to_viewport_top = element?.getBoundingClientRect().top || 0  // https://stackoverflow.com/a/35571753/9157799
    }
 </script>
 
 <svelte:window bind:scrollY/> <!-- https://svelte.dev/tutorial/svelte-window-bindings -->
 
-<div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: calc(100vh - {md_screen_sticky_options_distance_to_top}px)" bind:this={element}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | be careful with calc(): https://stackoverflow.com/q/34419813/9157799 | https://svelte.dev/tutorial/bind-this -->
+<div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: calc(100vh - {elemment_distance_to_viewport_top}px)" bind:this={element}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | be careful with calc(): https://stackoverflow.com/q/34419813/9157799 | https://svelte.dev/tutorial/bind-this -->
    <slot></slot>
 </div>

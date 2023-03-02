e6ee34c72490bdeae767e5a066c51cc7d35cc01c
M Imam Pratama <imambungo@gmail.com>
2023-03-03 03:36:58 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -249,20 +249,18 @@
          </div>
       </div>
    </main>
-   {#if all_repos.length != 0} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/tutorial/onmount -->
-      <footer class='text-center px-6 py-6 text-xs'>
-         <p class='flex gap-3 justify-center text-slate-600 decoration-slate-300 font-medium'>
-            <a class='underline' target='_blank' href='https://twitter.com/top1000repos'>Twitter</a>
-            <a class='underline' target='_blank' href='mailto:contact@top1000repos.com'>Contact</a>
-         </p>
-         <p class='mt-2 font-light text-slate-500'>
-            <span class='whitespace-nowrap decoration-slate-300'>
-               App <a class='underline' target="_blank" href="https://icons8.com/icon/103/star">icon</a> by <a class='underline' target="_blank" href="https://icons8.com">Icons8</a>.
-            </span>
-            <span class='md:hidden whitespace-nowrap decoration-slate-300'>
-               Option <a class='underline' target="_blank" href="https://icon-icons.com/icon/options/111009">burger</a> and <a class='underline' target='_blank' href='https://icon-icons.com/icon/close/111152'>close</a> button by <a class='underline' target='_blank' href='https://icon-icons.com/users/2j7Ni9Qgct0HtmWmLGnLj/icon-sets/'>akveo</a>.
-            </span>
-         </p>
-      </footer>
-   {/if}
+   <footer class='text-center px-6 py-6 text-xs'>
+      <p class='flex gap-3 justify-center text-slate-600 decoration-slate-300 font-medium'>
+         <a class='underline' target='_blank' href='https://twitter.com/top1000repos'>Twitter</a>
+         <a class='underline' target='_blank' href='mailto:contact@top1000repos.com'>Contact</a>
+      </p>
+      <p class='mt-2 font-light text-slate-500'>
+         <span class='whitespace-nowrap decoration-slate-300'>
+            App <a class='underline' target="_blank" href="https://icons8.com/icon/103/star">icon</a> by <a class='underline' target="_blank" href="https://icons8.com">Icons8</a>.
+         </span>
+         <span class='md:hidden whitespace-nowrap decoration-slate-300'>
+            Option <a class='underline' target="_blank" href="https://icon-icons.com/icon/options/111009">burger</a> and <a class='underline' target='_blank' href='https://icon-icons.com/icon/close/111152'>close</a> button by <a class='underline' target='_blank' href='https://icon-icons.com/users/2j7Ni9Qgct0HtmWmLGnLj/icon-sets/'>akveo</a>.
+         </span>
+      </p>
+   </footer>
 </div>

f00d5ab6b30e8bf0b352a013ebad6ba5b77afdc6
M Imam Pratama <imambungo@gmail.com>
2023-03-25 23:57:43 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -207,7 +207,7 @@
                </li>
             </ul>
          </nav>
-         <div class='flex flex-col gap-5 py-5'>
+         <div class='flex flex-col gap-6 py-5'>
             {#if all_repos.length == 0} <!-- https://stackoverflow.com/a/66080028/9157799 | https://svelte.dev/tutorial/onmount -->
                {#if !userAgent?.includes('Googlebot')}
                   <LoadingAnimation/>

cf0a6382c7098fc75b935b30db41a084e9e10117
M Imam Pratama <imambungo@gmail.com>
2023-09-05 17:27:27 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -175,6 +175,7 @@
 <svelte:head>
    <title>Top GitHub repositories ranking browser | Top 1000 Repos</title>
    <meta name="description" content="Browse the top 1000 GitHub repositories based on stars, pull requests, and issues. Hide repos that you don't need, they stay hidden every time you open the page. See the top pull requests and issues closed in the last 12 months of each repository.">
+   <link rel="canonical" href="https://top1000repos.com/">
 </svelte:head>
 
 <div class='min-h-screen flex flex-col   pl-[calc(100vw-100%)]' data-nosnippet> <!-- min-height and flex (combined with "grow" main) to keep footer at the bottom | https://stackoverflow.com/a/30293718/9157799 padding left for case when there's few item in blacklist (no scrollbar) | data-nosnippet: https://stackoverflow.com/a/72508996/9157799 -->

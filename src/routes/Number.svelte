b6ab68f49912970a11c58c027ed217bc65baa2ca
M Imam Pratama <imambungo@gmail.com>
2024-07-11 17:26:45 +0700

    s

--- src/routes/Number.svelte
+++ src/routes/Number.svelte
@@ -6,12 +6,8 @@
 
 <div class="text-sm pt-0.5 w-8 shrink-0 mr-2 text-gray-700 flex justify-end md:ml-0"> <!-- number | shrink: https://stackoverflow.com/a/45741742/9157799 -->
    {#if numbering == 'rank'}
-      <p class='cursor-help' title='rank based on the number of stars'>
-         {rank}
-      </p>
+      <p>{rank}</p>
    {:else}
-      <p class='cursor-help' title='numbering based on order'>
-         {order}
-      </p>
+      <p>{order}</p>
    {/if}
 </div>

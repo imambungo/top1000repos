15e127e72cdc2f1ff4caf319eab70c86629b58a1
M Imam Pratama <imambungo@gmail.com>
2024-07-21 11:55:24 +0700

    fix cloudflare pages build failed
    Error: The following routes were marked as prerenderable, but were not prerendered because they were not found while crawling your app:
      - /[[optional_parameter=sort_option]]

---
+++ src/routes/[[optional_parameter=sort_option]]/+page.server.js
@@ -0,0 +1,6 @@
+export function entries() { // https://kit.svelte.dev/docs/page-options#prerender-troubleshooting
+	return [
+		{ optional_parameter: 'based-on-pr' },
+		{ optional_parameter: 'based-on-issues' }
+	];
+}

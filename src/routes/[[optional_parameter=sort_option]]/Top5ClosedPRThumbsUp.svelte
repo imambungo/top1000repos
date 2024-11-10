58fcc4e4c767fe2712923322cd5b39492261d85e
M Imam Pratama <imambungo@gmail.com>
2024-11-10 17:36:33 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/Top5ClosedPRThumbsUp.svelte
+++ src/routes/[[optional_parameter=sort_option]]/Top5ClosedPRThumbsUp.svelte
@@ -1,16 +1,11 @@
 <script>
-	import { createBubbler } from 'svelte/legacy';
-
-	const bubble = createBubbler();
-
-	import { a_year_ago } from '$lib/date.js' // https://kit.svelte.dev/docs/project-structure#project-files
 	let { total_thumbs_up_of_top_5_closed_pr_since_1_year, html_url } = $props();
-
+	import { a_year_ago } from '$lib/date.js' // https://kit.svelte.dev/docs/project-structure#project-files
 	const top_5_closed_pr_url = `${html_url}/pulls?q=is%3Apr+sort%3Areactions-%2B1-desc+is%3Aclosed+closed%3A%3E${a_year_ago()}`
 </script>
 
 <div class='whitespace-nowrap'>
-	<a onpointerdown={bubble('pointerdown')} href={top_5_closed_pr_url} target='_blank' title='total 👍 of the top 5 closed (possibly merged) PRs of the last 12 months'> <!--  on:pointerdown ==> https://svelte.dev/tutorial/dom-event-forwarding  -->
+	<a href={top_5_closed_pr_url} target='_blank' title='total 👍 of the top 5 closed (possibly merged) PRs of the last 12 months'>
 		<span class='decoration-gray-300 underline'>Top 5 closed PRs</span>
 		(1 year) thumbs up:
 		<span class='decoration-gray-300 underline font-medium'>{total_thumbs_up_of_top_5_closed_pr_since_1_year}</span>

0174de8486e6072d6a451c4eac4bc9ac685bf65f
M Imam Pratama <imambungo@gmail.com>
2025-07-13 09:48:10 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/StargazersCount.svelte
+++ src/routes/[[optional_parameter=sort_option]]/StargazersCount.svelte
@@ -2,7 +2,12 @@
 	let { stargazers_count } = $props();
 
 	const readableNumber = number => { // https://stackoverflow.com/a/60988355/9157799
-		const formatter = Intl.NumberFormat('en', { notation: 'compact' })
+		let formatter
+		if (number >= 100000) { // https://github.com/imambungo/top1000repos/issues/12
+			formatter = Intl.NumberFormat('en', { notation: 'compact' })
+		} else { // https://stackoverflow.com/q/9461621/9157799#comment132576462_60988355
+			formatter = Intl.NumberFormat('en', { notation: 'compact', maximumFractionDigits: 1 })
+		}
 		let result = formatter.format(number)
 		return result.toLowerCase()
 	}

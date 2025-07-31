3ff01c1aa0c7409a66680949e3efda01d442c79f
M Imam Pratama <imambungo@gmail.com>
2025-07-31 09:56:26 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/+page.svelte
+++ src/routes/[[optional_parameter=sort_option]]/+page.svelte
@@ -308,15 +308,15 @@
             <SortOption sort_option={sort_option} handleChange={change_sort_option}/>
             <NumberingOption numbering={numbering} handleChange={change_numbering}/>
             <ExcludedTopicsOption excluded_topics={excluded_topics} excluded_repos_count={excluded_repos_count} excludeTopicToggle={excludeTopicToggle}/>
-            <div class='mt-6 w-[100%]'> <!-- parent container height will be set to auto by google adsense -->
+            <div class='mt-6 w-[100%]'> <!-- set width to 100% so the <ins> width:93% will work | parent container height will be set to auto by google adsense -->
                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2099788966207457" crossorigin="anonymous"></script>
                <!-- Ad -->
                <ins class="adsbygoogle"
-                  style="display:block;height:40vh;width:95%"
+                  style="display:block;height:40vh;width:93%"
                   data-ad-client="ca-pub-2099788966207457"
                   data-ad-slot="7664224172"
                   data-ad-format="rectangle"
-                  data-full-width-responsive="true"></ins> <!-- https://stackoverflow.com/q/26338357/9157799 -->
+                  data-full-width-responsive="true"></ins> <!-- https://stackoverflow.com/q/26338357/9157799 | set width to 93% to prevent horizontal scroll -->
                <script>
                   (adsbygoogle = window.adsbygoogle || []).push({});
                </script>

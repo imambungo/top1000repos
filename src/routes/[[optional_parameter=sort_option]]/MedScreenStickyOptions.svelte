c22326ae772e2d439fd9a12f41bd3a6980620342
M Imam Pratama <imambungo@gmail.com>
2025-09-08 09:27:51 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/MedScreenStickyOptions.svelte
+++ src/routes/[[optional_parameter=sort_option]]/MedScreenStickyOptions.svelte
@@ -2,7 +2,7 @@
    let { children } = $props() // https://svelte.dev/docs/svelte/snippet#Passing-snippets-to-components
    let scrollY = $state(0)  // window.scrollY | https://svelte.dev/tutorial/svelte-window-bindings
    let viewport_height = $state(0) // window.innerHeight | svelte can't detect window "undefined" if we try to use window.innerHeight directly (ups.. we can, in onMount (window or navigator is not found at server side))
-   let element = $state() // https://svelte.dev/tutorial/bind-this
+   let element = $state() // https://svelte.dev/tutorial/svelte/bind-this
    let max_height = $state(99999)
    $effect(() => {
       let trigger = scrollY
@@ -22,6 +22,6 @@
 
 <svelte:window bind:scrollY bind:innerHeight={viewport_height}/> <!-- https://svelte.dev/tutorial/svelte-window-bindings -->
 
-<div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: {max_height}px" bind:this={element}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | be careful with calc(): https://stackoverflow.com/q/34419813/9157799 | https://svelte.dev/tutorial/bind-this -->
+<div class="text-sm sticky top-0 py-4 flex flex-col gap-4 overflow-y-auto" style="max-height: {max_height}px" bind:this={element}> <!-- Tailwind doesn’t include any sort of client-side runtime, so class names need to be statically extractable at build-time, and can’t depend on any sort of arbitrary dynamic values that change on the client. Use inline styles for these situations.: https://v2.tailwindcss.com/docs/just-in-time-mode | be careful with calc(): https://stackoverflow.com/q/34419813/9157799 | https://svelte.dev/tutorial/svelte/bind-this -->
    {@render children?.()} <!-- https://svelte.dev/docs/svelte/@render#Optional-snippets -->
 </div>

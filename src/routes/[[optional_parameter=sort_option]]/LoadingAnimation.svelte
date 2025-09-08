a950d3416936942df5ffec021eedf208aed12194
M Imam Pratama <imambungo@gmail.com>
2025-09-08 09:26:26 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/LoadingAnimation.svelte
+++ src/routes/[[optional_parameter=sort_option]]/LoadingAnimation.svelte
@@ -1,14 +1,16 @@
 <script>
    import { onMount } from 'svelte'; // https://svelte.dev/tutorial/bind-this
 
-   let loading = $state()
+   let loading_text = $state('')
 
-   onMount(() => {  // https://svelte.dev/tutorial/bind-this
-      let num_of_dots = 1
+   onMount(() => {  // https://svelte.dev/docs/svelte/lifecycle-hooks#onMount
+      let num_of_dots = 0
       let change_num_of_dots = () => { // https://www.geeksforgeeks.org/how-to-create-frame-by-frame-animation-using-css-and-javascript/
-         loading.innerText = 'Hang on' + '.'.repeat(num_of_dots)
-         num_of_dots++;
-         if (num_of_dots > 3) num_of_dots = 0
+         if (num_of_dots < 3)
+            num_of_dots++
+         else
+            num_of_dots = 0
+         loading_text = 'Hang on' + '.'.repeat(num_of_dots)
       }
       change_num_of_dots() // https://stackoverflow.com/q/10563344/9157799
       const intervalID = setInterval(change_num_of_dots, 350) // https://developer.mozilla.org/en-US/docs/Web/API/setInterval
@@ -19,4 +21,4 @@
    })
 </script>
 
-<p bind:this={loading}></p>
+<p>{loading_text}</p>

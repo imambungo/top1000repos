6ff8d9f602629e54b37938a6c45af74a1363844f
M Imam Pratama <imambungo@gmail.com>
2025-09-15 14:47:10 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/+page.svelte
+++ src/routes/[[optional_parameter=sort_option]]/+page.svelte
@@ -159,7 +159,7 @@
       <title>Top 1000 GitHub repositories, updated daily, all on one page.</title>
       <link rel="canonical" href="https://top1000repos.com">
    {/if}
-   <meta name="description" content="Browse the top 1000 GitHub repositories. Sort them by stars, pull requests, and issues. Hide repos that you don't need. See the top pull requests and issues closed in the last 12 months of each repository.">
+   <meta name="description" content="Browse the top 1000 GitHub repositories. Sort them by stars, pull requests, and issues. Hide repos you don't need. See the top pull requests and issues closed in the last 12 months of each repository.">
 </svelte:head>
 
 <div class='min-h-screen flex flex-col   pl-[calc(100vw-100%)]' data-nosnippet> <!-- min-height and flex (combined with "grow" main) to keep footer at the bottom | https://stackoverflow.com/a/30293718/9157799 padding left for case when there's few item in hidden tab (no scrollbar) | data-nosnippet: https://stackoverflow.com/a/72508996/9157799 -->
@@ -176,7 +176,7 @@
             Top GitHub Repositories Ranking Browser
          </h1>
          <p class='text-slate-600 text-center w-full leading-snug' use:balancer={{ ratio: 0.60 }}> <!-- https://stackoverflow.com/q/34875725/9157799 -->
-            Browse the top 1000 GitHub repositories. Sort them by stars, pull requests, and issues. Hide repos that you don't need, they stay hidden every time you open the page. See the top pull requests and issues closed in the last 12 months of each repository.
+            Browse the top 1000 GitHub repositories. Sort them by stars, pull requests, and issues. Hide repos you don't need, they stay hidden every time you open the page. See the top pull requests and issues closed in the last 12 months of each repository.
          </p>
       </div>
    </header>

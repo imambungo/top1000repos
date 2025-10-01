529d74048a6da863795b54174be5b3fe48017a87
M Imam Pratama <imambungo@gmail.com>
2025-10-01 19:21:46 +0700

    s

--- src/routes/[[optional_parameter=sort_option]]/SortOptionButton.svelte
+++ src/routes/[[optional_parameter=sort_option]]/SortOptionButton.svelte
@@ -3,13 +3,13 @@
       sort_option, href, button_option,
       children // https://svelte.dev/docs/svelte/snippet#Passing-snippets-to-components
    } = $props()
-   const needed_when_using_a_instead_of_button = 'inline-block cursor-default hover:cursor-pointer'
+   const needed_when_using_a_instead_of_button = 'inline-block'
 </script>
 
 <a
   data-sveltekit-replacestate
   href='{href}'
-  class='{needed_when_using_a_instead_of_button} px-4 py-1 rounded-lg border tracking-tight {sort_option.option == button_option ? 'bg-gray-500 text-white hover:cursor-default' : 'text-gray-500'}'
+  class='{needed_when_using_a_instead_of_button} px-4 py-1 rounded-lg border tracking-tight {sort_option.option == button_option ? 'bg-gray-500 text-white cursor-default' : 'text-gray-500'}'
   onclick={() => sort_option.option = button_option}
 > <!-- https://kit.svelte.dev/docs/link-options#data-sveltekit-replacestate -->
    <span class='{sort_option.option == button_option ? "drop-shadow-[0_0_0.6px_rgba(256,256,256,0.5)]" : ""}'> <!-- https://stackoverflow.com/a/74753353/9157799 -->

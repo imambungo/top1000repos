391f1ad6c61f021078d93fb563c48f9cbb40b6b1
M Imam Pratama <imambungo@gmail.com>
2025-10-04 15:42:03 +0700

    add bookmarked_repos to repos and its filter function

--- src/routes/[[optional_parameter=sort_option]]/repos_filter_functions.js
+++ src/routes/[[optional_parameter=sort_option]]/repos_filter_functions.js
@@ -1,28 +1,21 @@
-export const filter_blacklisted_repos_based_on_current_tab = (repos, repo_id_blacklist, current_tab) => {
-   const filter_out_blacklisted_repos = (repos, repo_id_blacklist) => {
-      const not_blacklisted_repo = repo => {
-         if (repo_id_blacklist.includes(repo.id))
-            return false
-         else
-            return true
-      }
-      return repos.filter(not_blacklisted_repo)
+export const filter_repos_based_on_current_tab = (repos, bookmared_repos_ids, repo_id_blacklist, current_tab) => {
+   const filter_only_repos_not_in_list = (repos, repo_id_list) => {
+      return repos.filter(repo => !repo_id_list.includes(repo.id))
    }
 
-   const filter_only_blacklisted_repos = (repos, repo_id_blacklist) => {
-      const is_blacklisted_repo = repo => {
-         if (repo_id_blacklist.includes(repo.id))
-            return true
-         else
-            return false
-      }
-      return repos.filter(is_blacklisted_repo)
+   const filter_only_repos_in_list = (repos, repo_id_list) => {
+      return repos.filter(repo => repo_id_list.includes(repo.id))
    }
 
-   if (current_tab == 'explore')
-      return filter_out_blacklisted_repos(repos, repo_id_blacklist)
-   if (current_tab == 'blacklist')
-      return filter_only_blacklisted_repos(repos, repo_id_blacklist)
-   else
+   if (current_tab == 'explore') {
+      repos = filter_only_repos_not_in_list(repos, bookmared_repos_ids)
+      repos = filter_only_repos_not_in_list(repos, repo_id_blacklist)
       return repos
+   } else if (current_tab == 'bookmark') {
+      return filter_only_repos_in_list(repos, bookmared_repos_ids)
+   } else if (current_tab == 'blacklist') {
+      return filter_only_repos_in_list(repos, repo_id_blacklist)
+   } else {
+      return repos
+   }
 }

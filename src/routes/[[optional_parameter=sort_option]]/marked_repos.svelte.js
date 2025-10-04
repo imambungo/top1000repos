b86cd8bc3f021048e11ac6acf49854c54971320f
M Imam Pratama <imambungo@gmail.com>
2025-10-04 14:17:42 +0700

    add bookmarked_repos to marked_repos.svelte.js

--- src/routes/[[optional_parameter=sort_option]]/marked_repos.svelte.js
+++ src/routes/[[optional_parameter=sort_option]]/marked_repos.svelte.js
@@ -1,20 +1,21 @@
 import { local_storage as ls } from '$lib/local_storage.js'
 
-const create_hidden_repos = () => {
+const create_marked_repos = (mark) => {
    let ids = $state([])
 
    return {
       get ids() { return ids },
-      load_from_local_storage: () => ids = ls.getItem('repo_id_blacklist') || [], // to be run at client init hook: https://svelte.dev/docs/kit/hooks#Shared-hooks-init
-      hide_repo: repo_id => {
+      load_from_local_storage: () => ids = ls.getItem(mark) || [], // to be run at client init hook: https://svelte.dev/docs/kit/hooks#Shared-hooks-init
+      add: repo_id => {
          ids = [...ids, repo_id]
-         ls.setItem('repo_id_blacklist', ids)
+         ls.setItem(mark, ids)
       },
-      unhide_repo: repo_id => {
+      remove: repo_id => {
          ids = ids.filter(id => id != repo_id)
-         ls.setItem('repo_id_blacklist', ids)
+         ls.setItem(mark, ids)
       }
    }
 }
 
-export const hidden_repos = create_hidden_repos() // create a singleton by running the function inside this file | https://nodejs.org/docs/latest/api/modules.html#caching
+export const hidden_repos = create_marked_repos('repo_id_blacklist') // create a singleton by running the function inside this file | https://nodejs.org/docs/latest/api/modules.html#caching
+export const bookmarked_repos = create_marked_repos('bookmarked_repos_ids')

2814be64e348b5e0ee409573da622fa576089774
M Imam Pratama <imambungo@gmail.com>
2025-10-04 16:22:15 +0700

    add bookmark to TabButtons

--- src/routes/[[optional_parameter=sort_option]]/TabButtons.svelte
+++ src/routes/[[optional_parameter=sort_option]]/TabButtons.svelte
@@ -10,6 +10,13 @@
    {/if}
 </li>
 <li class="mr-2">
+   {#if current_tab.tab == 'bookmark'}
+      <a href="#top" class="inline-block p-4 border-b-2 text-gray-700 border-blue-500" aria-current="page">Bookmark ({repos.count.bookmark_tab})</a>
+   {:else}
+      <a href="#top" class="inline-block p-4 border-b-2 text-gray-500 hover:text-gray-600 hover:border-gray-300" onclick={() => current_tab.tab = 'bookmark'}>Bookmark ({repos.count.bookmark_tab})</a>
+   {/if}
+</li>
+<li class="mr-2">
    {#if current_tab.tab == 'blacklist'}
       <a href="#top" class="inline-block p-4 border-b-2 text-gray-700 border-blue-500" aria-current="page">Hidden ({repos.count.hidden_tab})</a>
    {:else}
